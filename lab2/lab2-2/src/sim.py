e228ccf41cd425f0be18e57bb48a9216bcbcb980
hewo233 <2842137843@qq.com>
2024-11-18 18:48:22 +0800

    pdf

--- lab2/lab2-2/src/sim.py
+++ lab2/lab2-2/src/sim.py
@@ -4,6 +4,6 @@ def simulate_dfa(dfa, input_string):
         if symbol in dfa.transitions.get(current_state, {}):
             current_state = dfa.transitions[current_state][symbol]
         else:
-            return False  # 输入中有不可识别的符号，导致无法转移状态
+            return False 
     return current_state in dfa.accept_states
 
